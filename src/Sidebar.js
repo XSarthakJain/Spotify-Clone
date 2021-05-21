17933eff52ecc81df2594877b794021414c85167
Sarthak Jain <sarthakjain31797@gmail.com>
2021-05-22 03:17:50 +0530

    Creating Sidebar template

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Sidebar.css';
+
+let Sidebar = () => {
+    return (
+        <React.Fragment>
+            <div className="sidebar">
+                <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify Image" className="sidebar_logo"/>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Sidebar;

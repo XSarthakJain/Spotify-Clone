8c050680bd3a2d7ec75673cc2f5d8e196489ec97
Sarthak Jain <sarthakjain31797@gmail.com>
2021-05-22 18:43:47 +0530

    Grab user name and user pic from spotify and display it

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './css/header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core';
-
+import { useDataLayerValue } from './DataLayer';
 let Header = ()=>{
+    let [{user},dispatch] = useDataLayerValue();
     return (
         <React.Fragment>
             <div className="header">
@@ -13,8 +14,8 @@ let Header = ()=>{
                 </div>
 
                 <div className="header-right">
-                    <Avatar src="" alt="RQ"/>
-                    <h4>User name</h4>
+                    <Avatar src={user?.images[0]?.url} alt={user?.display_name}/>
+                    <h4>{ user?.display_name }</h4>
                 </div>
             </div>
         </React.Fragment>

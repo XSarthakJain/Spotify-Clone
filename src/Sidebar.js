36fe02bd8971d27cf87359119efec03a2872b37f
Sarthak Jain <sarthakjain31797@gmail.com>
2021-05-22 16:19:32 +0530

    Display all playlist in the Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './css/Sidebar.css';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import SearchIcon from '@material-ui/icons/Search';
+import { useDataLayerValue } from './DataLayer';
 
 let Sidebar = () => {
+    const [{ playlists },dispatch] = useDataLayerValue();
+
     return (
         <React.Fragment>
             <div className="sidebar">
                 <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify Image" className="sidebar_logo"/>
+
+                <SidebarOption title="Home" Icon={HomeIcon}/>
+                <SidebarOption title="Search" Icon={SearchIcon}/>
+                <SidebarOption title="Your Library" Icon={LibraryMusicIcon}/>
+
+                <br/>
+                <strong className="sidebar_title">
+                    PLAYLIST
+                </strong>
+                <hr/>
+                
+                {playlists?.items?.map((playlist,ind) => (
+                    <SidebarOption title={playlist.name} key={ind}/>
+                ))}
+
             </div>
         </React.Fragment>
     );
